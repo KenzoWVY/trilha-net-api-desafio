165d27ba8c867186028bd1f34404df0b9cfcbc56
KenzoWVY <kenzowendell@gmail.com>
2025-08-27 03:31:26 -0300

    Remover TODO

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -85,8 +85,8 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            // IMPLEMENTADO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
+            // IMPLEMENTADO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
             tarefaBanco.Titulo = tarefa.Titulo;
             tarefaBanco.Descricao = tarefa.Descricao;
             tarefaBanco.Data = tarefa.Data;
@@ -106,7 +106,7 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefaBanco == null)
                 return NotFound();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            // IMPLEMENTADO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
             _context.Tarefas.Remove(tarefaBanco);
             _context.SaveChanges();
 
